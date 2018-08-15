3fdba11699aa6c9b9e2d0ba756896a112be9d213
gdilts <42283412+gdilts@users.noreply.github.com>
2018-08-15 14:53:32 -0700

    initial commit

---
+++ src/main/java/edu/gcu/bootcamp/java/gdilts/helloworld/App.java
@@ -0,0 +1,13 @@
+package edu.gcu.bootcamp.java.gdilts.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
